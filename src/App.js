32c2ca91d02d5b91a86b05226ca443606e5946b8
Денис Котелев <deniskotelev@MBP-Denis.Dlink>
2023-10-08 23:49:10 +0300

    feat

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useCallback, useEffect } from 'react';
+import axios from 'axios';
+
+const ulr = 'http://localhost:8080'
+
+const interval = 1000 * 15;
 
 function App() {
+
+  const requset = useCallback(() => {
+    try {
+      axios(ulr)
+    } catch (error) {
+      console.log('error')
+    }
+  }, []);
+
+  useEffect(() => {
+    requset();
+
+    setInterval(() => {
+      requset()
+    }, interval);
+  }, [requset]);
+
   return (
     <div className="App">
       <header className="App-header">
